8cdb573da6788099b2ae657072932afcf11ee269
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-05-19 12:35:19 +0200

    adding color wheel

---
+++ script.js
@@ -0,0 +1,10 @@
+// this is default version
+var colorPicker = new iro.ColorPicker('#picker');
+
+// and this is version where we can play with our wheel and add whatever property we want
+// var colorPicker = new iro.ColorPicker("#picker", {
+//     // Set the size of the color picker
+//     width: 320,
+//     // Set the initial color to pure red
+//     color: "#f00"
+//   });
