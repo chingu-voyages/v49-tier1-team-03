79470cf9f68ece7fe393c887641d0c41d418786d
Diego Raffa <41127155+Diegoireland1975@users.noreply.github.com>
2024-05-26 21:45:04 +0100

    Sprint 3 colour selection - fix: layout, color palette (#24)
    * Ignore DS store files
    
    * Add HTML boilerplate and basic structure
    
    * Add @media query for responsive design
    
    * Add CSS code to visualise the wireframe layout
    
    Add white borders, padding and margin to visualise box model. Will be
    removed later after further desing work.
    
    * added box sizing border box
    
    * Move footer element into body element
    
    * Adding .js file
    
    * adding color wheel
    
    * added poppin font
    
    * added logo on top and is sticky on scroll
    
    * added a change of opacity on hover on the h1 in the header with a transition
    
    * changed the name of the div logo on the top, with the header tag because is more semantic
    
    * put the header outside the page div
    
    * added a container as utility class and used for the header
    
    * re added the border and box padding for debugging
    
    * changed the font size for h1 in the header
    
    * Update desktop layout
    
    * Update desktop layout and refactor CSS class names
    
    - Desktop layout displays content in a row.
    - Refactor CSS class names. Changed the class page to page-container to
    better reflect what this class does - ie a flexbox container.
    
    * Add input field and js logic to get user's input
    
    - Add js logic to get user input by adding id selectors to access html
    input field
    - Add js function according to iro documentation to update the color
    wheel and also user input field accordingly
    
    https://iro.js.org/guide.html#color-picker-events
    https://iro.js.org/guide.html#selected-color-api
    
    * Adjust headline to align with logo,so not centered
    
    Add headline-container class for better specificity for headline
    - Remove center alignment
    - Remove padding so that the headline is now aligned with the logo
    
    * Update desktop layout - add space between element
    
    Add space between colour picker on the right and the content on the left
    side.
    
    * starting to add the input field for color combination
    
    * starting to style the dropdown color
    
    * added a custom arrow in the select tag and removed the default one
    
    * added color palette box
    
    * put the color wheel text as h1 and Visualize and Combine Color Like a Pro as h2 following the figma file
    
    * reduced the width of the imputs fields
    
    * changed height of the header and margins for h1 and h2
    
    * changed weight forn of h1
    
    * enlarge with of imput fields until tablet view
    
    * enlarge with of imput fields until tablet view
    
    * updated font size for h1 and h2
    
    * changed max width for imput field on desktop
    
    * adjust the margin top and bottom for the imputs
    
    * changed the font weight for imputs
    
    * font family inherit for input
    
    * implementing the diffrent color wheels based on the user selection combination
    
    * Delete .vscode/settings.json
    
    ---------
    
    Co-authored-by: flora8heart <9201125+flora8heart@users.noreply.github.com>
    Co-authored-by: flora8heart <flora8heart@users.noreply.github.com>
    Co-authored-by: dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // this is default version
-var colorPicker = new iro.ColorPicker(".picker");
+// var colorPicker = new iro.ColorPicker(".picker");
 
 // and this is version where we can play with our wheel and add whatever property we want
-// var colorPicker = new iro.ColorPicker(".picker", {
-//     // Set the size of the color picker
-//     width: 320,
-//     // Set the initial color to pure red
-//     color: "#f00"
-//   });
+var colorPicker = new iro.ColorPicker(".picker", {
+    // Set the size of the color picker
+    width: 320,
+    // Set the initial color to pure red
+    color: "#f719ff",
+    id: 'default'
+  });
 
 // Get user selection by referencing the id selector "hexInput"
 var hexInput = document.getElementById("hexInput");
@@ -25,3 +26,75 @@ colorPicker.on(["color:init", "color:change"], function (color) {
 hexInput.addEventListener("change", function () {
   colorPicker.color.hexString = this.value;
 });
+
+let box2 = document.querySelector(".box2")
+let box3 = document.querySelector(".box3");
+let box4 = document.querySelector(".box4");
+
+
+let combination = document.querySelector("#combination");
+combination.addEventListener("change", function() {
+  console.log(this.value)
+  if(this.value === 'Triadic') { 
+    box3.style.display = 'block';
+    box4.style.display = 'none';
+    box2.style.backgroundColor = '#73ff19'
+    if (colorPicker.id === 'default') {
+      document.querySelector("#default").style.display = 'none'
+      // document.querySelector("#tetradic").style.display = 'none'
+      const colorPickerTriadic = new iro.ColorPicker(".triadic", {
+        // color picker options
+        // Option guide: https://iro.js.org/guide.html#color-picker-options
+        width: 320,
+        // Pure red, green and blue
+        colors: [
+          "rgb(255, 0, 0)",
+          "rgb(0, 255, 0)",
+          "rgb(0, 0, 255)",
+        ],
+        handleRadius: 9,
+        borderWidth: 1,
+        borderColor: "#fff",
+        id: 'triadic'
+      });
+    }
+    // console.log(colorPicker.id)
+  } else if (this.value === 'Tetradic') {
+    box3.style.display = 'block';
+    box4.style.display = 'block';
+    box2.style.backgroundColor = '#73ff19'
+    document.querySelector("#triadic").style.display = 'none'
+    const colorPickerTetradic = new iro.ColorPicker(".tetradic", {
+      // color picker options
+      // Option guide: https://iro.js.org/guide.html#color-picker-options
+      width: 320,
+      // Pure red, green and blue
+      colors: [
+        "rgb(255, 0, 0)",
+        "rgb(0, 255, 0)",
+        "rgb(0, 0, 255)",
+        "rgb(0, 0, 255)",
+        "rgb(255, 0, 238)"
+      ],
+      handleRadius: 9,
+      borderWidth: 1,
+      borderColor: "#fff",
+      id: 'tetradic'
+    });
+  } else if (this.value === 'Analogous') {
+    box3.style.display = 'block';
+    box4.style.display = 'none';
+    box2.style.backgroundColor = '#73ff19'
+  } else if (this.value === 'Monochromatic') {
+    box3.style.display = 'none';
+    box4.style.display = 'none';
+    box2.style.backgroundColor = '#DD00E5'
+  } else if (this.value === 'Complementary') {
+    box2.style.backgroundColor = '#73ff19'
+    box3.style.display = 'none';
+    box4.style.display = 'none';
+  } else {
+    box3.style.display = 'none';
+    box4.style.display = 'none';
+  }
+})
