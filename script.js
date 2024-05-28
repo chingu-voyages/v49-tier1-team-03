c64bbef21b0c9876057f9f6cb8400ddae5ffb3cb
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-05-28 16:32:39 +0200

    Setting the color wheels based on the user inputed choice

--- script.js
+++ script.js
@@ -10,6 +10,47 @@ var colorPicker = new iro.ColorPicker(".picker", {
     id: 'default'
   });
 
+  const colorPickerTriadic = new iro.ColorPicker(".triadic", {
+    // color picker options
+    // Option guide: https://iro.js.org/guide.html#color-picker-options
+    width: 320,
+    // Pure red, green and blue
+    colors: [
+      "rgb(255, 0, 0)",
+      "rgb(0, 255, 0)",
+      "rgb(0, 0, 255)",
+    ],
+    handleRadius: 9,
+    borderWidth: 1,
+    borderColor: "#fff",
+    id: 'triadic'
+  });
+
+  const colorPickerTetradic = new iro.ColorPicker(".tetradic", {
+    // color picker options
+    // Option guide: https://iro.js.org/guide.html#color-picker-options
+    width: 320,
+    // Pure red, green and blue
+    colors: [
+      "rgb(255, 0, 0)",
+      "rgb(0, 255, 0)",
+      "rgb(0, 0, 255)",
+      "rgb(0, 0, 255)",
+      "rgb(255, 0, 238)"
+    ],
+    handleRadius: 9,
+    borderWidth: 1,
+    borderColor: "#fff",
+    id: 'tetradic'
+  });
+
+
+  // selecting the color wheels
+  let standard = document.querySelector(".picker");
+  let triadic = document.querySelector(".triadic");
+  let tetradic = document.querySelector(".tetradic");
+
+
 // Get user selection by referencing the id selector "hexInput"
 var hexInput = document.getElementById("hexInput");
 
@@ -27,74 +68,75 @@ hexInput.addEventListener("change", function () {
   colorPicker.color.hexString = this.value;
 });
 
+
+// Selecting boxes of color pallete from the DOM
 let box2 = document.querySelector(".box2")
 let box3 = document.querySelector(".box3");
 let box4 = document.querySelector(".box4");
 
 
-let combination = document.querySelector("#combination");
-combination.addEventListener("change", function() {
+// Selecting choices from the DOM
+let combinations = document.querySelector("#combinations");
+
+// Setting changes on each option, so the right wheel apears after specific option is selected
+combinations.addEventListener("change", function() {
   console.log(this.value)
-  if(this.value === 'Triadic') { 
-    box3.style.display = 'block';
-    box4.style.display = 'none';
-    box2.style.backgroundColor = '#73ff19'
-    if (colorPicker.id === 'default') {
-      document.querySelector("#default").style.display = 'none'
-      // document.querySelector("#tetradic").style.display = 'none'
-      const colorPickerTriadic = new iro.ColorPicker(".triadic", {
-        // color picker options
-        // Option guide: https://iro.js.org/guide.html#color-picker-options
-        width: 320,
-        // Pure red, green and blue
-        colors: [
-          "rgb(255, 0, 0)",
-          "rgb(0, 255, 0)",
-          "rgb(0, 0, 255)",
-        ],
-        handleRadius: 9,
-        borderWidth: 1,
-        borderColor: "#fff",
-        id: 'triadic'
-      });
-    }
-    // console.log(colorPicker.id)
-  } else if (this.value === 'Tetradic') {
-    box3.style.display = 'block';
-    box4.style.display = 'block';
-    box2.style.backgroundColor = '#73ff19'
-    document.querySelector("#triadic").style.display = 'none'
-    const colorPickerTetradic = new iro.ColorPicker(".tetradic", {
-      // color picker options
-      // Option guide: https://iro.js.org/guide.html#color-picker-options
-      width: 320,
-      // Pure red, green and blue
-      colors: [
-        "rgb(255, 0, 0)",
-        "rgb(0, 255, 0)",
-        "rgb(0, 0, 255)",
-        "rgb(0, 0, 255)",
-        "rgb(255, 0, 238)"
-      ],
-      handleRadius: 9,
-      borderWidth: 1,
-      borderColor: "#fff",
-      id: 'tetradic'
-    });
-  } else if (this.value === 'Analogous') {
-    box3.style.display = 'block';
-    box4.style.display = 'none';
-    box2.style.backgroundColor = '#73ff19'
-  } else if (this.value === 'Monochromatic') {
-    box3.style.display = 'none';
-    box4.style.display = 'none';
-    box2.style.backgroundColor = '#DD00E5'
-  } else if (this.value === 'Complementary') {
-    box2.style.backgroundColor = '#73ff19'
-    box3.style.display = 'none';
-    box4.style.display = 'none';
+
+  if(this.value === 'Triadic') {   // Triadic
+
+      box3.style.display = 'block';
+      box4.style.display = 'none';
+      box2.style.backgroundColor = '#73ff19'
+
+      standard.style.display = "none"
+      tetradic.style.display = "none";
+      triadic.style.display = "block"; 
+
+  } else if (this.value === 'Tetradic') {  // Tetradic
+
+      box3.style.display = 'block';
+      box4.style.display = 'block';
+      box2.style.backgroundColor = '#73ff19'
+
+      standard.style.display = "none";
+      triadic.style.display = "none";
+      tetradic.style.display = "block";
+
+  } else if (this.value === 'Analogous') {  // Analogous
+
+      box3.style.display = 'block';
+      box4.style.display = 'none';
+      box2.style.backgroundColor = '#73ff19'
+
+      standard.style.display = "none";
+      tetradic.style.display = "none";
+      triadic.style.display = "block";
+
+  } else if (this.value === 'Monochromatic') { // Monochromatic
+
+      box3.style.display = 'none';
+      box4.style.display = 'none';
+      box2.style.backgroundColor = '#DD00E5'
+
+      standard.style.display = "block"
+      triadic.style.display = "none"
+      tetradic.style.display = "none" 
+
+
+  } else if (this.value === 'Complementary') { // Complementary
+
+      box2.style.backgroundColor = '#73ff19'
+      box3.style.display = 'none';
+      box4.style.display = 'none';
+
+      standard.style.display = "block"
+      triadic.style.display = "none"
+      tetradic.style.display = "none"
+
   } else {
-    box3.style.display = 'none';
-    box4.style.display = 'none';
+
+      box3.style.display = 'none';
+      box4.style.display = 'none';
+
   }
 })
