bcbabf42b7bc2932c8650adef072315dd25e06ba
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-05-28 10:36:41 +0100

    Add colors to color wheel and color palette

--- script.js
+++ script.js
@@ -13,18 +13,13 @@ var colorPicker = new iro.ColorPicker(".picker", {
 // Get user selection by referencing the id selector "hexInput"
 var hexInput = document.getElementById("hexInput");
 
-// Initialise user_hex variable
-let user_hex = 0;
-
 // When there is a color change ie user selects a color on the wheel, update the input field with the selected color.
 // https://iro.js.org/guide.html#color-picker-events
 colorPicker.on(["color:init", "color:change"], function (color) {
   // Using the selected color: https://iro.js.org/guide.html#selected-color-api
   hexInput.value = color.hexString; // the input field is updated with the color's hex string.
-  console.log(color.hexString); // Debugging - prints selected value in console
-  console.log(hexInput.value); // Debugging - prints selected value in console
-  user_hex = color.hexString;
-  console.log(user_hex); // Debugging - prints user's choice
+  console.log("color.hexString", color.hexString); // Debugging - prints selected value in console
+  console.log("hexInput.value", hexInput.value); // Debugging - prints selected value in console
 });
 
 //When the user types something in the input field and hit enter -> The "change" event is triggered - the color wheel will be updated with user's color
@@ -32,10 +27,47 @@ hexInput.addEventListener("change", function () {
   colorPicker.color.hexString = this.value;
 });
 
-// Get user color harmony selection
-let color_harmony = document.getElementBy;
+// Initialise user_hex variable
+let userHexCode = 0;
+let colorHarmony = 0;
 
-// Get suggestion from groq ai and add to color array.
+// since the following funciton will called twice ie get color harmony and call groq ai. Better to create a separate function.
+
+// Get user color harmony selection
+document.addEventListener("DOMContentLoaded", getColorHarmony);
+
+combinations.addEventListener("change", getColorHarmony);
+
+function getColorHarmony() {
+  // If there are colors in the color array, reset the array to only store the first color aka the active color. This is so that when it comes to creating dynamic color palette, we're not including the previous color suggestion if the webpages hasn't been refreshed yet.
+  if (colorPicker.colors.length != 0) {
+    colorPicker.colors = [colorPicker.colors[0]];
+  }
+
+  console.log("colorPicker.colors.length", colorPicker.colors.length); //debugging. prints current color array length
+
+  let combinations = document.getElementById("combinations");
+  console.log("combinations", combinations);
+
+  console.log("colorPicker.colors.length", colorPicker.colors.length);
+
+  // Get user color harmony selection
+  colorHarmony = combinations.value;
+  console.log("colorHarmony", colorHarmony);
+
+  userHexCode = hexInput.value;
+  console.log("userHexCode", userHexCode);
+
+  // Get suggestion from groq ai
+
+  // Add color suggestion to color array
+  // add a color to the color picker
+  // this will add the color to the end of the colors array
+  colorPicker.addColor("#ff85e0"); // colors are temporary. will be changed to groq ai suggestion
+  colorPicker.addColor("#d6f6ff");
+
+  console.log("colorPicker.colors.length", colorPicker.colors.length); //debugging. prints current color array length
+}
 
 /*
 Dynamic Colour Palette
@@ -45,13 +77,15 @@ const colorList = document.getElementById("colorPalette");
 
 // https://iro.js.org/guide.html#color-picker-events
 // When there is a change on the colour wheel, html element is updated and displays colors from color array
-colorPicker.on(["mount", "color:change"], function () {
+colorPicker.on(["mount", "color:change", "color:init"], function () {
   colorPalette.innerHTML = "";
+  console.log("colors array", colorPicker.colors);
   colorPicker.colors.forEach((color) => {
     const hexString = color.hexString;
+    const index = color.index; //debugging
     colorPalette.innerHTML += `
       <li>
-        <div class="swatch" style="background: ${hexString}">${hexString}</div>
+        <div class="swatch" style="background: ${hexString}">${index}: ${hexString}</div>
       </li>
     `;
   });
